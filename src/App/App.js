ab403878e8f650366a7b97339c1a95c923cc7909
bforbiggy <AndyHeCS@protonmail.com>
2022-06-25 09:16:26 -0400

    Basic site setup

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import logo from './dagojo.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Top 10 reacts">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
