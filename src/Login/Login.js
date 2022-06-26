50967e32690fc4f99c93fb94c4466c089584eea7
bforbiggy <AndyHeCS@protonmail.com>
2022-06-26 09:17:00 -0400

    Actually reads in username + password

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,24 @@
 import './Login.css';
 import React from 'react';
 
+
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 'Code something decent.'
+            username: '',
+            password: ''
         };
         this.sendPost = this.sendPost.bind(this)
+        this.fieldChange = this.fieldChange.bind(this)
+    }
+
+    fieldChange(e) {
+        var callerId = e.target.id;
+        if (callerId === "username")
+            this.setState({ username: e.target.value });
+        else if (callerId === "password")
+            this.setState({ password: e.target.value });
     }
 
     sendPost() {
@@ -16,18 +27,19 @@ class Login extends React.Component {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ username: 'username', password: 'password' })
+            body: JSON.stringify(this.state)
         };
+
         fetch('http://162.84.178.38:3001/auth', requestOptions)
             .then(response => response.json())
             .then(data => console.log(data.token))
     }
 
     render() {
-        return <div className="test">
-            <input type="text" placeholder="username here" id="username"></input>
+        return <div className="loginPage">
+            <input type="text" placeholder="Username" value={this.state.username} onChange={this.fieldChange} id="username" />
             <br />
-            <input type="text" placeholder="password here" id="password"></input>
+            <input type="text" placeholder="Password" value={this.state.password} onChange={this.fieldChange} id="password" />
             <br />
             <button onClick={this.sendPost}>Login</button>
         </div>
