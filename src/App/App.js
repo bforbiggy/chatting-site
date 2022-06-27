f335cae891c5b47bbb4cac33cfb6077228c42f1f
Uhifa <matzhang29@gmail.com>
2022-06-26 21:12:50 -0400

    monkey code

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,31 @@
 import logo from './dagojo.webp';
 import './App.css';
 import Login from '../Login/Login'
-import React, { useState } from 'react';
+import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: localStorage.getItem("token")
+    }
+    this.setToken = this.setToken.bind(this)
+  }
+
+  setToken(newToken) {
+    console.log("updated token", newToken)
+    localStorage.setItem("token", newToken)
+    this.setState({
+      token: newToken
+    })
+  }
+
   render() {
     return (
       <div className="Top 10 reacts">
-        <header className="Header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
-          <Login />
-
+          <Login token={this.state.token} setToken={this.setToken} />
         </header>
       </div>
     );
